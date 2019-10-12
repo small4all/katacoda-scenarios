cbd7da27d49bebd36222e111b43495410b3c7056
Steffen Mall <steffen.mall@exxeta.com>
2019-10-12 11:18:06 +0200

    adapt kubernetes scenario

--- devops-fundamentals/kubernetes-basic-scenario/prepareWorkspace.sh
+++ devops-fundamentals/kubernetes-basic-scenario/prepareWorkspace.sh
@@ -1,17 +1,17 @@
 
-read -p "Bitte gebe deinen User Token ein: " USER_TOKEN
-echo "Dein User Token lautet:" $USER_TOKEN
+#read -p "Bitte gebe deinen User Token ein: " USER_TOKEN
+#echo "Dein User Token lautet:" $USER_TOKEN
 
-read -p "Bitte gebe deinen Namespace ein: " NAMESPACE
-echo "Dein Namespace lautet:" $NAMESPACE
+#read -p "Bitte gebe deinen Namespace ein: " NAMESPACE
+#echo "Dein Namespace lautet:" $NAMESPACE
 
-echo "Konfiguriere kubeconfig..."
+#echo "Konfiguriere kubeconfig..."
 
-kubectl config set-cluster k8s --server="${SERVER}"
-kubectl config set clusters.k8s.certificate-authority-data ${CERTIFICATE_AUTHORITY_DATA}
-kubectl config set-credentials ${USER} --token="${USER_TOKEN}"
-kubectl config set-context ${USER} --cluster=k8s --user=${USER} --namespace=${NAMESPACE}
-kubectl config use-context ${USER}
+#kubectl config set-cluster k8s --server="${SERVER}"
+#kubectl config set clusters.k8s.certificate-authority-data ${CERTIFICATE_AUTHORITY_DATA}
+#kubectl config set-credentials ${USER} --token="${USER_TOKEN}"
+#kubectl config set-context ${USER} --cluster=k8s --user=${USER} --namespace=${NAMESPACE}
+#kubectl config use-context ${USER}
 
-echo "Teste kubeconfig..."
-kubectl version 
+#echo "Teste kubeconfig..."
+#kubectl version 
